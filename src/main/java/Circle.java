55571cdbfefa24b275154c96df68dbd0c7b0bd75
Nikolay <orato@bk.ru>
2023-10-29 18:04:13 +0300

    added three new classes

---
+++ src/main/java/Circle.java
@@ -0,0 +1,7 @@
+public class Circle {
+    public static void main(String[] args) {
+        double radius = 5;
+        double area = Math.PI * (radius * radius);
+        System.out.println("Circle area = " + area);
+    }
+}
---
+++ src/main/java/Triangle.java
@@ -0,0 +1,12 @@
+public class Triangle {
+    public static void main(String[] args) {
+        double sideA = 5.6;
+        double sideB = 7.4;
+        double sideC = 9.2;
+        double S = (sideA+sideB+sideC)/2;
+        double area = Math.sqrt(S*(S-sideA)*(S-sideB)*(S-sideC));
+        System.out.println("Triangle area = " + area);
+
+
+    }
+}
