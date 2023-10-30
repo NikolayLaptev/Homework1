53eecaf74f1314e0227076844fda922370fc80ab
Nikolay <orato@bk.ru>
2023-10-30 13:50:46 +0300

    Разделил объявление переменной и логику метода пустой строкой

--- src/main/java/Circle.java
+++ src/main/java/Circle.java
@@ -1,8 +1,8 @@
 public class Circle {
     public static void main(String[] args) {
         double radius = 5;
-
         double area = Math.PI * (radius * radius);
+
         System.out.println("Circle area = " + area);
     }
 }
