a96724744261eb90bddbf3ccdbcac05e3ab04480
Nikolay <orato@bk.ru>
2023-10-30 13:32:09 +0300

    Разделил объявление переменной и логику метода пустой строкой

--- src/main/java/Square.java
+++ src/main/java/Square.java
@@ -2,6 +2,7 @@ public class Square {
     public static void main(String[] args) {
         int side = 5;
         int area = side * side;
+
         System.out.println("Square area = " + area);
     }
 }
--- src/main/java/Triangle.java
+++ src/main/java/Triangle.java
@@ -3,10 +3,9 @@ public class Triangle {
         double sideA = 5.6;
         double sideB = 7.4;
         double sideC = 9.2;
+
         double S = (sideA+sideB+sideC)/2;
         double area = Math.sqrt(S*(S-sideA)*(S-sideB)*(S-sideC));
         System.out.println("Triangle area = " + area);
-
-
     }
 }
